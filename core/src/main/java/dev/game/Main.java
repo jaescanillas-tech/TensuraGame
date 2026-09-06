0ddbd195d4bbb2e4a4c556fe228137630a0bd446
javier <ja.escanillas@duocuc.cl>
2026-09-06 18:13:56 -0300

    prueba de push

--- core/src/main/java/dev/game/Main.java
+++ core/src/main/java/dev/game/Main.java
@@ -1,7 +1,7 @@
 package dev.game;
 
 import com.badlogic.gdx.Game;
-
+//prueba de psuh
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
